b89ae884c370b81552c80ceacbede8ffde895694
Criss <crisserpl2@gmail.com>
2018-01-01 22:51:18 +0100

    Textured particles

--- particles/main.cpp
+++ particles/main.cpp
@@ -2,6 +2,7 @@
 
 #include "maths/vectors.h"
 #include "maths/Mat4.h"
+#include "tga/tga.h"
 #include "utility/Clock.h"
 #include "CameraController.h"
 
@@ -27,8 +28,8 @@ void main() {\n\
 	velocity[idx] *= 1 - .99f * dt;\n\
 	if (attractorActive) {\n\
 		float dist = distance(attractorPosition, position[idx].xyz);\n\
-		float power = 10000.f / max(1.f, 0.01f * pow(dist, 1.5f));\n\
-		velocity[idx] += vec4(normalize(attractorPosition - position[idx].xyz), 0.f) * power * dt;\n\
+		float acc = 10000.f / max(1.f, 0.01f * pow(dist, 1.5f));\n\
+		velocity[idx] += vec4(normalize(attractorPosition - position[idx].xyz), 0.f) * acc * dt;\n\
 	}\n\
 	position[idx] += velocity[idx] * dt;\n\
 }\
@@ -55,6 +56,7 @@ layout(triangle_strip, max_vertices = 4) out;\n\
 \n\
 in vec3 vs_color[];\n\
 out vec3 fs_color;\n\
+out vec2 fs_txCoords;\n\
 \n\
 layout(location = 1) uniform mat4 projection;\n\
 const vec2 offsets[4] = {\n\
@@ -63,6 +65,7 @@ const vec2 offsets[4] = {\n\
 void main() {\n\
 	fs_color = vs_color[0];\n\
 	for (int i = 0; i < 4; ++i) {\n\
+		fs_txCoords = offsets[i];\n\
 		vec4 pos = gl_in[0].gl_Position;\n\
 		pos.xy += .5f * (offsets[i] - vec2(0.5f));\n\
 		gl_Position = projection * pos;\n\
@@ -74,39 +77,27 @@ void main() {\n\
 const char * const FS_SRC = "\
 #version 430 core\n\
 in vec3 fs_color;\n\
+in vec2 fs_txCoords;\n\
 out vec4 color;\n\
+layout(location = 2) uniform sampler2D txSampler;\n\
 \n\
 void main() {\n\
-	color = vec4(fs_color, 1.f);\n\
+	color = texture(txSampler, fs_txCoords) * vec4(fs_color, 1.f);\n\
 }\
 ";
 
+const char * const PARTICLE_TEXTURE_PATH = "particle.tga";
+
 enum Bindings { PositionBuffer = 0, VelocityBuffer = 1 };
 enum CsUniformLoc { DeltaTime = 0, AttractorActive = 1, AttractorPosition = 2 };
-enum GeneralShaderUniformLoc { View = 0, Projection = 1 };
+enum GeneralShaderUniformLoc { View = 0, Projection = 1, Texture = 2 };
 enum AttrLoc { Position = 0, Velocity = 1 };
 
 GLuint makeCs(const char * const _src);
 GLuint makeGeneralShader(const char * const _vs, const char * const _gs, const char * const _fs);
+GLuint loadTexture(const char * const _path);
 
-void checkErrors()
-{
-	GLenum err;
-	while ((err = glGetError()) != GL_NO_ERROR)
-	{
-		switch (err)
-		{
-		case GL_INVALID_ENUM: printf("err invalid enum\n"); break;
-		case GL_INVALID_VALUE: printf("err invalid val\n"); break;
-		case GL_INVALID_OPERATION: printf("err invalid op\n"); break;
-		case GL_STACK_OVERFLOW: printf("err stack overflow\n"); break;
-		case GL_STACK_UNDERFLOW: printf("err stack underflow\n"); break;
-		case GL_OUT_OF_MEMORY: printf("err out of mem\n"); break;
-		case GL_INVALID_FRAMEBUFFER_OPERATION: printf("err invalid fb op\n"); break;
-		case GL_CONTEXT_LOST: printf("err ctx lost\n"); break;
-		}
-	}
-}
+void checkErrors();
 
 int main(int, char**)
 {
@@ -115,7 +106,7 @@ int main(int, char**)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	const fhl::Vec2u WIN_SIZE{800u, 600u};
+	const fhl::Vec2u WIN_SIZE{1280, 720u};
 	auto window = glfwCreateWindow(WIN_SIZE.x(), WIN_SIZE.y(), "particles", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
@@ -127,13 +118,14 @@ int main(int, char**)
 	std::srand(997);
 
 	const std::size_t PARTICLE_CNT = 1u << 21; // ~2M
+	const float GRAVITY_POINT_DISTANCE_FROM_CAM = 800.f;
 
 	std::vector<fhl::Vec4f> positions;
 	positions.reserve(PARTICLE_CNT);
 	for (float x = 0; x < (1u << 7); ++x)
 		for (float y = 0; y < (1u << 7); ++y)
-			for (float z = 0; z < (1u << 7); ++z)
-				positions.push_back(fhl::Vec4f{x, y, z, 1.f} + fhl::Vec4f{WIN_SIZE/2, fhl::Vec2f::zero()});
+			for (float z = 0; -z < (1u << 7); --z)
+				positions.push_back(fhl::Vec4f{x, y, z, 1.f});
 	std::vector<fhl::Vec4f> velocities(PARTICLE_CNT, fhl::Vec4f::zero());
 
 	glEnable(GL_BLEND);
@@ -166,11 +158,16 @@ int main(int, char**)
 	glVertexAttribPointer(AttrLoc::Velocity, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(fhl::Vec4f::valueType), (void *)0);
 	glEnableVertexAttribArray(AttrLoc::Velocity);
 
+	GLuint particleTex = loadTexture(PARTICLE_TEXTURE_PATH);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, particleTex);
+
 	const fhl::Mat4f projection = fhl::Mat4f::perspective(45.f, -float(WIN_SIZE.x()) / WIN_SIZE.y(), 0, 1e3f);
 	
-	Camera cam{fhl::Vec3f{828.f, 328.f, 350.f}};
+	//Camera cam{fhl::Vec3f{828.f, 328.f, 350.f}};
+	Camera cam{fhl::Vec3f{64.f, 64.f, GRAVITY_POINT_DISTANCE_FROM_CAM + 64.f}};
 	CameraController camController({&cam});
-	camController.setTranslationSpeed(3.f);
+	camController.setTranslationSpeed(10.f);
 
 	std::map<int, int> keyStates;
 	fhl::Clock clock;
@@ -181,7 +178,7 @@ int main(int, char**)
 
 		const float dt = clock.restart();
 		bool mblPressed = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
-		const fhl::Vec3f attractorPosition = cam.getPosition() + -cam.getDirectionVector() * 800.f;
+		const fhl::Vec3f attractorPosition = cam.getPosition() + -cam.getDirectionVector() * GRAVITY_POINT_DISTANCE_FROM_CAM * .75f;
 
 		glUseProgram(cs);
 		glUniform1f(CsUniformLoc::DeltaTime, dt);
@@ -193,10 +190,12 @@ int main(int, char**)
 		glUseProgram(shader);
 		glUniformMatrix4fv(GeneralShaderUniformLoc::View, 1, GL_FALSE, cam.getView().data());
 		glUniformMatrix4fv(GeneralShaderUniformLoc::Projection, 1, GL_FALSE, projection.data());
+		glUniform1i(GeneralShaderUniformLoc::Texture, 0);
 
 		fhl::Vec2lf currentMouse;
 		glfwGetCursorPos(window, &currentMouse.x(), &currentMouse.y());
 		camController.processMousePosition(currentMouse);
+
 		for (int k : {GLFW_KEY_W, GLFW_KEY_S, GLFW_KEY_A, GLFW_KEY_D})
 			keyStates[k] = glfwGetKey(window, k);
 		camController.processKeyStates(keyStates);
@@ -293,4 +292,48 @@ GLuint makeGeneralShader(const char * const _vs, const char * const _gs, const c
 	}
 
 	return program;
+}
+
+GLuint loadTexture(const char * const _path)
+{
+	tTGA  tga;
+	if (!load_TGA(&tga, _path)) 
+		throw std::runtime_error("Could not load particle texture!");
+	GLuint tex{};
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		(tga.alpha) ? (GL_RGB) : (GL_RGB8),
+		tga.width,
+		tga.height,
+		0,
+		(tga.alpha) ? (GL_RGBA) : (GL_RGB),
+		GL_UNSIGNED_BYTE,
+		tga.data
+	);
+	free_TGA(&tga);
+	return tex;
+}
+
+void checkErrors()
+{
+	GLenum err{};
+	while ((err = glGetError()) != GL_NO_ERROR)
+	{
+		switch (err)
+		{
+		case GL_INVALID_ENUM: printf("err invalid enum\n"); break;
+		case GL_INVALID_VALUE: printf("err invalid val\n"); break;
+		case GL_INVALID_OPERATION: printf("err invalid op\n"); break;
+		case GL_STACK_OVERFLOW: printf("err stack overflow\n"); break;
+		case GL_STACK_UNDERFLOW: printf("err stack underflow\n"); break;
+		case GL_OUT_OF_MEMORY: printf("err out of mem\n"); break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION: printf("err invalid fb op\n"); break;
+		case GL_CONTEXT_LOST: printf("err ctx lost\n"); break;
+		}
+	}
 }
